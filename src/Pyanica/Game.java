d10af0a70355321092535f02287c1736c1bdd030
ANTON <asta111ken@bk.ru>
2020-10-23 15:33:51 +0300

    Initial commit

---
+++ src/Pyanica/Game.java
@@ -0,0 +1,56 @@
+package Pyanica;
+import java.util.ArrayList;
+public class Game {
+    public static void main(String[] args) {
+        int[] a1 = new int[5];
+        for (int k = 0; k < a1.length; k++) {
+            a1[k] = ((int)(Math.random() * 10));
+            {
+                System.out.print(a1[k] + " ");
+            }
+        }
+        int[] a2 = new int[5];
+        System.out.print("\n");
+        for (int l = 0; l < a2.length; l++) {
+            a2[l] = ((int)((Math.random() * 10)));
+            {
+                System.out.print(a2[l] + " ");
+            }
+        }
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        ArrayList<Integer> arr2 = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            arr1.add(a1[i]);
+            arr2.add(a2[i]);
+        }
+
+        int count = 0;
+        boolean res = false;
+
+        System.out.print("\n");
+
+        for (int i = 0; i < 106; i++) {
+            count++;
+            if (arr1.get(0) > arr2.get(0) || ((arr1.get(0) == 0) && (arr2.get(0) == 9))) {
+                arr1.add(arr1.get(0));
+                arr1.add(arr2.get(0));
+            } else if (arr1.get(0) < arr2.get(0) || ((arr1.get(0) == 9) && (arr2.get(0) == 0))) {
+                arr2.add(arr2.get(0));
+                arr2.add(arr1.get(0));
+            }
+            arr1.remove(0);
+            arr2.remove(0);
+
+            if (arr1.size() == 0) {
+                System.out.println("Second " + count);
+                res = true;
+                break;
+            } else if (arr2.size() == 0) {
+                System.out.println("First " + count);
+                res = true;
+                break;
+            }
+        }
+        if (!res) System.out.println("Botva!");
+    }
+}
